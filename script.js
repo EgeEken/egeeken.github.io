188f3cec8d60e409fcf929cff979dbbb99390ad4
Ege <96302110+EgeEken@users.noreply.github.com>
2024-02-19 17:04:31 +0100

    video text fix, category open fix

--- script.js
+++ script.js
@@ -1,16 +1,8 @@
 document.addEventListener('DOMContentLoaded', function () {
-    const tagSelector = document.getElementById('tag-selector');
-    const categories = document.querySelectorAll('.category');
-    const programmingButton = document.getElementById('programming-btn');
-    const musicButton = document.getElementById('music-btn');
-    const otherButton = document.getElementById('other-btn');
-
-    programmingButton.addEventListener('click', () => showCategory('programming'));
-    musicButton.addEventListener('click', () => showCategory('music'));
-    otherButton.addEventListener('click', () => showCategory('other'));
 
     function showCategory(category) {
-        const portfolio = document.getElementById('portfolio');
+        const tagSelector = document.getElementById('tag-selector');
+        const categories = document.querySelectorAll('.category');
         categories.forEach(cat => {
             if (cat.id === category) {
                 if (cat.style.display === 'none') {
@@ -28,31 +20,13 @@ document.addEventListener('DOMContentLoaded', function () {
                 cat.style.display = 'none';
             }
         });
-        portfolio.classList.toggle('open');
     }
 
+    const programmingButton = document.getElementById('programming-btn');
+    const musicButton = document.getElementById('music-btn');
     showCategory('programming');
-
-    const videoProjects = document.querySelectorAll('.category video');
-
-    videoProjects.forEach(video => {
-        video.addEventListener('loadeddata', function () {
-            const projectInfo = video.parentElement.querySelector('.project-info');
-            projectInfo.style.width = '80%';
-            projectInfo.style.textAlign = 'center';
-            projectInfo.style.color = '#fff';
-            projectInfo.style.opacity = '0';
-            projectInfo.style.transition = 'opacity 0.3s ease-in-out';
-
-            video.parentElement.addEventListener('mouseenter', function () {
-                projectInfo.style.opacity = '1';
-            });
-
-            video.parentElement.addEventListener('mouseleave', function () {
-                projectInfo.style.opacity = '0';
-            });
-        });
-    });
+    programmingButton.addEventListener('click', () => showCategory('programming'));
+    musicButton.addEventListener('click', () => showCategory('music'));
 });
 
 function filterProjects(tags) {
